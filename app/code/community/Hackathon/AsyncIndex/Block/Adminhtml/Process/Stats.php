89b0b6d37f57f6ffaf3cb6c2a8ed18f34a9eb942
Flyingmana <flyingmana@googlemail.com>
2014-05-11 12:51:37 +0200

    show simple stats about pending indexes

--- app/code/community/Hackathon/AsyncIndex/Block/Adminhtml/Process/Stats.php
+++ app/code/community/Hackathon/AsyncIndex/Block/Adminhtml/Process/Stats.php
@@ -2,5 +2,19 @@
 
 class Hackathon_AsyncIndex_Block_Adminhtml_Process_Stats extends Mage_Adminhtml_Block_Template
 {
-    
+
+
+    public function getPendingEventsPerProcess()
+    {
+        $pCollection = Mage::getSingleton('index/indexer')->getProcessesCollection();
+
+        $result = array();
+
+        /** @var Mage_Index_Model_Process $process */
+        foreach ($pCollection as $process) {
+            $result[ $process->getIndexerCode() ] = $process->getUnprocessedEventsCollection()->count();
+        }
+
+        return $result;
+    }
 }
