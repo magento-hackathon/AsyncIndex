bfd1329a53c65ea592e4ae3d4f5fa206131872ef
Oprean Dan Stefan <dan@designxenon.com>
2014-05-17 07:01:42 +0300

    Add new cron method.

--- app/code/community/Hackathon/AsyncIndex/Model/Observer.php
+++ app/code/community/Hackathon/AsyncIndex/Model/Observer.php
@@ -2,6 +2,9 @@
 
 class Hackathon_AsyncIndex_Model_Observer extends Mage_Core_Model_Abstract
 {
+    /**
+     * Executes manually scheduled reindex
+     */
     public function schedule_index()
     {
         $scheduledJob = Mage::getModel('cron/schedule')->getCollection()
@@ -10,16 +13,56 @@ class Hackathon_AsyncIndex_Model_Observer extends Mage_Core_Model_Abstract
 
         $indexer = 'tag_aggregation'; //fallback - if not set this should be the fastest on every shop
 
-        if ( $scheduledJob->getStatus() != 'success' )
-        {
+        if ($scheduledJob->getStatus() != 'success') {
             $indexer = intval($scheduledJob->getMessages());
         }
 
         $indexProcess = Mage::getSingleton('index/indexer')->getProcessByCode($indexer);
 
-        if ($indexProcess)
-        {
+        if ($indexProcess) {
             $indexProcess->reindexEverything();
+        }
+
+    }
+
+    /**
+     * Indexes a specific number of events
+     *
+     * @throws Exception
+     */
+    public function unprocessed_events_index()
+    {
+
+        if (!Mage::getStoreConfig('system/asyncindex/auto_index')) {
+            return null;
+        }
+
+        /** @var $resourceModel Mage_Index_Model_Resource_Process */
+        $resourceModel = Mage::getResourceSingleton('index/process');
+
+        $resourceModel->beginTransaction();
+
+        try {
+            $pCollection = Mage::getSingleton('index/indexer')->getProcessesCollection();
+            /** @var Mage_Index_Model_Process $process */
+            foreach ($pCollection as $process) {
+                $process->setMode(Mage_Index_Model_Process::MODE_SCHEDULE);
+                $eventLimit            = (int)Mage::getStoreConfig('system/asyncindex/event_limit');
+                $unprocessedCollection = $process->getUnprocessedEventsCollection()->setPageSize($eventLimit);
+
+                /** @var Mage_Index_Model_Event $unprocessedEvent */
+                foreach ($unprocessedCollection as $unprocessedEvent) {
+                    $process->processEvent($unprocessedEvent);
+                    $unprocessedEvent->save();
+                }
+                if (count(Mage::getResourceSingleton('index/event')->getUnprocessedEvents($process)) === 0) {
+                    $process->changeStatus(Mage_Index_Model_Process::STATUS_PENDING);
+                }
+            }
+            $resourceModel->commit();
+        } catch (Exception $e) {
+            $resourceModel->rollBack();
+            throw $e;
         }
 
     }
