dd609ec1771175cf76ad0a4e1b003295c477dd51
Fabian Schmengler <fschmengler@sgh-it.eu>
2014-11-23 22:19:46 +0100

    Replace block rewrite with observers.

--- app/code/community/Hackathon/AsyncIndex/Model/Observer.php
+++ app/code/community/Hackathon/AsyncIndex/Model/Observer.php
@@ -1,7 +1,115 @@
 <?php
 
-class Hackathon_AsyncIndex_Model_Observer extends Mage_Core_Model_Abstract
+class Hackathon_AsyncIndex_Model_Observer
 {
+    /**
+     * @var bool
+     */
+    protected $_shouldLoadUnprocessedEventCount = false;
+
+    /**
+     * @see event adminhtml_block_html_before
+     * @param Varien_Event_Observer $observer
+     * @return Hackathon_AsyncIndex_Model_Observer
+     */
+    public function extendIndexProcessGrid(Varien_Event_Observer $observer)
+    {
+        /** @var Mage_Adminhtml_Block_Template $block */
+        $block = $observer->getBlock();
+        if ($block instanceof Mage_Index_Block_Adminhtml_Process_Grid) {
+            $this->_addEventCountColumnTo($block);
+            $this->_changeActions($block);
+        }
+        return $this;
+    }
+    /**
+     * Add count of indexed data to grid
+     *
+     * @param Mage_Index_Block_Adminhtml_Process_Grid $grid
+     * @return Hackathon_AsyncIndex_Model_Observer
+     */
+    protected function _addEventCountColumnTo(Mage_Index_Block_Adminhtml_Process_Grid $grid)
+    {
+        $grid->addColumnAfter('event_count', array(
+                'header'   => Mage::helper('index')->__('Event Count'),
+                'width'    => '80',
+                'index'    => 'event_count',
+                'sortable' => false
+            ), 'ended_at'
+        );
+        return $this;
+    }
+    /**
+     * Change action column to dropdown with additional actions
+     * - schedule reindex
+     * - schedule partial reindex
+     * 
+     * @param Mage_Index_Block_Adminhtml_Process_Grid $grid
+     * @return Hackathon_AsyncIndex_Model_Observer
+     */
+    protected function _changeActions(Mage_Index_Block_Adminhtml_Process_Grid $grid)
+    {
+        $grid->getColumn('action')->setActions(array(
+                array(
+                    'caption' => Mage::helper('index')->__('Reindex Data'),
+                    'url'     => array('base' => '*/*/reindexProcess'),
+                    'field'   => 'process'
+                ),
+                array(
+                    'caption' => Mage::helper('index')->__('Schedule Reindex'),
+                    'url'     => array('base' => 'asyncindex/asyncindex/index'),
+                    'params'  => array('_current' => true, '_secure' => false),
+                    'field'   => 'process'
+                ),
+                array(
+                    'caption' => Mage::helper('index')->__('Schedule partial index'),
+                    'url'     => array('base' => 'asyncindex/asyncindex/schedulePartial'),
+                    'params'  => array('_current' => true, '_secure' => false),
+                    'field'   => 'process'
+                ),
+            ));
+        return $this;
+    }
+    /**
+     * Set flag to add unprocessed event count after load.
+     * Uses core_block_abstract_to_html_before because this is the only event fired before
+     * grid is prepared and collection loaded. Cannot use collection flag because collection
+     * is not instantiated yet.
+     * 
+     * @see event core_block_abstract_to_html_before
+     * @param Varien_Event_Observer $observer
+     * @return Hackathon_AsyncIndex_Model_Observer
+     */
+    public function prepareCollectionForGrid(Varien_Event_Observer $observer)
+    {
+        /* @var $block Mage_Core_Block_Abstract $block */
+        $block = $observer->getBlock();
+        if ($block instanceof Mage_Index_Block_Adminhtml_Process_Grid) {
+            $this->_shouldLoadUnprocessedEventCount = true;
+        }
+        return $this;
+    }
+    /**
+     * Add event count to process collection if flag has been set
+     * 
+     * @see event process_collection_load_after
+     * @param Varien_Event_Observer $observer
+     * @return Hackathon_AsyncIndex_Model_Observer
+     */
+    public function addEventCountToCollection(Varien_Event_Observer $observer)
+    {
+        /* @var $processCollection Mage_Index_Model_Resource_Process_Collection */
+        $processCollection = $observer->getProcessCollection();
+        if ($this->_shouldLoadUnprocessedEventCount) {
+            /* @var $process Mage_Index_Model_Process */
+            foreach ($processCollection as $process) {
+                $process->setEventCount($process->getUnprocessedEventsCollection()->count());
+            }
+        }
+        
+        return $this;
+    }
+
     /**
      * Executes manually scheduled reindex
      */
